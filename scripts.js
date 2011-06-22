2528dee4b94efec4565da21a0a9accd49aeefb46
gboudreau <gboudreau@pommepause.com>
2011-06-22 14:03:36 -0400

    New option to disable notifications, new check on length of User Key

--- scripts.js
+++ scripts.js
@@ -17,6 +17,12 @@ function savePrefs() {
 	// save preferences
 	localStorage['userkey'] = $("#userkey").val();
 	localStorage['colorCodeUpload'] = $("#color_code_upload")[0].checked;
+	localStorage['showNotifications'] = $("#show_notifications")[0].checked;
+	
+	if ($("#userkey").val().length != 16) {
+        $("#status").html('<span style="color: #F00">' + t("invalid_user_key") + '</span>');
+        return;
+	}
 	
 	chrome.extension.sendRequest({action : 'loadUsage'}, function() {});
 
@@ -30,7 +36,6 @@ function show() {
 	
 	var userkey = localStorage['userkey'];
 	if (!userkey || userkey == null || userkey.length == 0) {
-    	console.log(userkey)
 		$('#loading').css('display', 'none');
 		$('#needs_config').css('display', 'block');
 		return;
@@ -230,7 +235,7 @@ function dateFormat(d, include_time) {
 	if (typeof d == 'string') {
 		d = new Date(d);
 	}
-	return d.getFullYear()+'-'+(d.getMonth()+1 < 10 ? '0'+(d.getMonth()+1) : (d.getMonth()+1))+'-'+(d.getDate() < 10 ? '0'+d.getDate() : d.getDate()) + (include_time ? (' ' + d.getHours() + ':' + d.getMinutes()) : '');
+	return d.getFullYear()+'-'+(d.getMonth()+1 < 10 ? '0'+(d.getMonth()+1) : (d.getMonth()+1))+'-'+(d.getDate() < 10 ? '0'+d.getDate() : d.getDate()) + (include_time ? (' ' + (d.getHours() < 10 ? '0'+d.getHours() : d.getHours()) + ':' + (d.getMinutes() < 10 ? '0'+d.getMinutes() : d.getMinutes())) : '');
 }
 
 var units = new Array("B","KB","MB","GB");
